4852eec5699be0370124519f23694c580f50fab0
Himasopn <129391898+Himasopn@users.noreply.github.com>
2024-10-09 23:49:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ module.exports = {
   KOYEB_APP_NAME: process.env.KOYEB_APP_NAME || '',
   KOYEB: toBool(process.env.KOYEB) || false,
   HEROKU: toBool(process.env.HEROKU) || false,
-  TERMUX: toBool(process.env.TERMUX) || false,
+  TERMUX: toBool(process.env.TERMUX) || true,
   DATABASE_URL: DATABASE_URL,
   DATABASE:
        DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
